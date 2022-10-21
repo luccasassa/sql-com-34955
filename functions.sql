f62de93448509ef32c26fca36f5e9719b0766bb4
Luca <dev.luccasassa@gmail.com>
2022-10-21 02:20:14 -0300

    v1.2 (Funciones)

--- functions.sql
+++ functions.sql
@@ -1,23 +1,25 @@
+#FUNCION 1
+
 delimiter //
 CREATE FUNCTION buscar_por_palabra (palabra VARCHAR(20)) RETURNS INT
 READS SQL DATA
 BEGIN
-	DECLARE resultado INT;
-    SELECT COUNT(*) INTO resultado FROM producto WHERE predesc_producto LIKE CONCAT('%',palabra,'%');
-    RETURN resultado;
+DECLARE resultado INT;
+SELECT COUNT(*) INTO resultado FROM producto WHERE predesc_producto LIKE CONCAT('%',palabra,'%');
+RETURN resultado;
 END//
 delimiter ;
 
-SELECT buscar_por_palabra('inalámbrico') AS 'Cantidad de productos';
+#FUNCION 2
 
 delimiter //
-CREATE FUNCTION promedio_precio_auriculares (promedio INT) RETURNS INT
+CREATE FUNCTION cantidad_categorias (cantidad INT) RETURNS INT
 READS SQL DATA
 BEGIN
-	DECLARE resultado INT;
-    SELECT AVG(precio_producto) INTO resultado FROM producto;
-    RETURN resultado;
+DECLARE resultado INT;
+SELECT nombre_categoria cat INTO resultado
+FROM categoria cat JOIN producto p ON cat.id_categoria = p.id_producto
+WHERE p.id_categoria = cat.id_categoria;
+RETURN resultado;
 END//
 delimiter ;
-
-SELECT promedio_precio_auriculares();
