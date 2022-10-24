c27eea4c4ec76f37668a138192fce4ce2dd5dc41
Luca <dev.luccasassa@gmail.com>
2022-10-24 16:32:56 -0300

    v1.3 (Funciones)

--- functions.sql
+++ functions.sql
@@ -1,5 +1,8 @@
-#FUNCION 1
+USE redstore;
+DROP function IF EXISTS buscar_por_palabra;
+DROP function IF EXISTS precio_mas_iva;
 
+#FUNCION 1
 delimiter //
 CREATE FUNCTION buscar_por_palabra (palabra VARCHAR(20)) RETURNS INT
 READS SQL DATA
@@ -11,15 +14,12 @@ END//
 delimiter ;
 
 #FUNCION 2
-
 delimiter //
-CREATE FUNCTION cantidad_categorias (cantidad INT) RETURNS INT
+CREATE FUNCTION precio_mas_iva (calcular INT) RETURNS INT
 READS SQL DATA
 BEGIN
 DECLARE resultado INT;
-SELECT nombre_categoria cat INTO resultado
-FROM categoria cat JOIN producto p ON cat.id_categoria = p.id_producto
-WHERE p.id_categoria = cat.id_categoria;
-RETURN resultado;
+SELECT precio_producto INTO resultado FROM producto WHERE id_producto = calcular;
+RETURN resultado + (resultado * 0.30);
 END//
 delimiter ;
