2062ee473eb4e65afcb54d9c24578042c6a200cf
Luca <dev.luccasassa@gmail.com>
2022-11-01 21:04:37 -0300

    v1.6 (SEGUNDA ENTREGA)

--- sp.sql
+++ sp.sql
@@ -1,19 +1,20 @@
 USE redstore;
-DROP PROCEDURE IF EXISTS crear_nuevo_producto;
 DROP PROCEDURE IF EXISTS ordenar_productos_segun;
+DROP PROCEDURE IF EXISTS crear_nuevo_producto;
+DROP PROCEDURE IF EXISTS filtrar_productos_segun;
 
 #SP 1
 DELIMITER // 
 CREATE PROCEDURE ordenar_productos_segun (IN campo INT, IN orden INT) 
 BEGIN 
-    SELECT * 
-    FROM producto 
-    ORDER BY 
-    CASE WHEN campo = 1 AND orden = 1 THEN id_producto END, 
-    CASE WHEN campo = 1 AND orden = 2 THEN id_producto END DESC, 
-    CASE WHEN campo = 2 AND orden = 1 THEN nombre_producto END, 
-    CASE WHEN campo = 2 AND orden = 2 THEN nombre_producto END DESC, 
-    CASE WHEN campo = 3 AND orden = 1 THEN vista_producto END, 
+	SELECT * 
+	FROM producto 
+	ORDER BY 
+	CASE WHEN campo = 1 AND orden = 1 THEN id_producto END, 
+	CASE WHEN campo = 1 AND orden = 2 THEN id_producto END DESC, 
+	CASE WHEN campo = 2 AND orden = 1 THEN nombre_producto END, 
+	CASE WHEN campo = 2 AND orden = 2 THEN nombre_producto END DESC, 
+	CASE WHEN campo = 3 AND orden = 1 THEN vista_producto END, 
 	CASE WHEN campo = 3 AND orden = 2 THEN vista_producto END DESC, 
 	CASE WHEN campo = 4 AND orden = 1 THEN predesc_producto END, 
 	CASE WHEN campo = 4 AND orden = 2 THEN predesc_producto END DESC, 
@@ -23,12 +24,12 @@ BEGIN
 	CASE WHEN campo = 6 AND orden = 2 THEN id_categoria END DESC, 
 	CASE WHEN campo = 7 AND orden = 1 THEN stock_producto END, 
 	CASE WHEN campo = 7 AND orden = 2 THEN stock_producto END DESC, 
-    CASE WHEN campo = 8 AND orden = 1 THEN precio_producto END, 
+	CASE WHEN campo = 8 AND orden = 1 THEN precio_producto END, 
 	CASE WHEN campo = 8 AND orden = 2 THEN precio_producto END DESC; 
 END // 
 DELIMITER ;
 
-# CALL ordenar_productos (5, 1);
+# CALL ordenar_productos_segun (5, 1);
 
 #SP 2
 DELIMITER // 
@@ -40,7 +41,7 @@ CREATE PROCEDURE crear_nuevo_producto (
 	IN categoria INT, 
 	IN stock INT, 
 	IN precio INT, 
-    IN img VARCHAR(1000)) 
+	IN img VARCHAR(1000)) 
 BEGIN 
 	INSERT INTO producto (
     nombre_producto, 
@@ -50,17 +51,19 @@ BEGIN
     id_categoria, 
     stock_producto, 
     precio_producto, 
-    icrear_nuevo_productomg_producto) 
+    img_producto) 
 	VALUES (nombre, vista, predescripcion, descripcion, categoria, stock, precio, img); 
 END // 
 DELIMITER ;
 
-# CALL nuevo_producto ('ASD-ASD', 'Prducto ASD-ASD', 'Nuevo producto asd-asd store procedure', 'lorem*10', 4, 1, 9999, 'https://luccasassa.netlify.app/');
+# CALL crear_nuevo_producto ('ASD-ASD', 'Prducto ASD-ASD', 'Nuevo producto asd-asd store procedure', 'lorem*10', 4, 1, 9999, 'https://luccasassa.netlify.app/');
 
 #SP 3
-delimiter // 
+DELIMITER // 
 CREATE PROCEDURE filtrar_productos_segun (buscar VARCHAR(20)) 
 BEGIN 
-SELECT * FROM producto WHERE predesc_producto LIKE CONCAT('%',buscar,'%'); 
+	SELECT * FROM producto WHERE predesc_producto LIKE CONCAT('%',buscar,'%'); 
 END // 
-delimiter ;
+DELIMITER ;
+
+# CALL filtrar_productos_segun ('inalámbrico');
